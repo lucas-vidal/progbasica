28ced887d3a9eb6b5b3fb98b9dd149e4bc91c347
Lucas Vidal <lucasvidal0904@gmail.com>
2020-05-13 00:35:01 -0300

    canvas con JS

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,8 +1,17 @@
 var d =document.getElementById("dibujito");
 var lienzo = d.getContext("2d")
+var lineas = 30;
+var l = 0;
+var yi, xf;
 
-dibujarLinea("red", 10,300,220,10);
-dibujarLinea("blue", 300, 10, 10, 220);
+while( l < lineas)
+{
+    yi = 10 * l;
+    xf = 10 * ( l + 1 );
+    dibujarLinea("green", 0, yi, xf, 300);   
+    console.log("Linea " + l);
+    l = l + 1;
+}
 
 function dibujarLinea (color, xinicial, yinicial, xfinal, yfinal)
 {
