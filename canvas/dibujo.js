8092f54bc2a4d69657dbeee77c383fd160148c19
Lucas Vidal <lucasvidal0904@gmail.com>
2020-05-14 01:45:50 -0300

    canvas

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,17 +1,10 @@
-var d =document.getElementById("dibujito");
-var lienzo = d.getContext("2d")
-var lineas = 30;
-var l = 0;
-var yi, xf;
+var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click", dibujoPorClick);
 
-while( l < lineas)
-{
-    yi = 10 * l;
-    xf = 10 * ( l + 1 );
-    dibujarLinea("green", 0, yi, xf, 300);   
-    console.log("Linea " + l);
-    l = l + 1;
-}
+var d =document.getElementById("dibujito");
+var lienzo = d.getContext("2d");
+
 
 function dibujarLinea (color, xinicial, yinicial, xfinal, yfinal)
 {
@@ -21,4 +14,25 @@ function dibujarLinea (color, xinicial, yinicial, xfinal, yfinal)
     lienzo.lineTo(xfinal, yfinal);
     lienzo.stroke();
     lienzo.closePath;
+}
+function dibujoPorClick()
+{
+    var xxx = parseInt(texto.value);
+    alert("No me toques ahi " + texto.value);
+
+    var lineas = 30;
+var l = 0;
+var yi, xf;
+
+for( l = 0; 1 < lineas; l++)
+{
+    yi = 10 * l;
+    xf = 10 * ( l + 1 );
+    dibujarLinea("green", 0, yi, xf, 300);   
+    console.log("Linea " + l);
+    l = l + 1;
+}
+
+dibujarLinea(colorcito, 1,1,1,299);  
+dibujarLinea(colorcito, 1,299,299,299);  
 }
